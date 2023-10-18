f544880ec8f9ed20ad4277fbbbbe381a8460a0ad
Zane Chaplin <zchaplin@ucsc.edu>
2023-10-18 12:06:35 -0700

    Maybe Finished 3

--- src/main.ts
+++ src/main.ts
@@ -14,19 +14,26 @@ const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 
 const context = canvas.getContext("2d")!;
 
-context!.fillStyle = "green";
+context.fillStyle = "green";
 
-context!.fillRect(0, 0, 300, 150);
+context.fillRect(0, 0, 300, 150);
 
-let clearB = document.querySelector<HTMLButtonElement>("#clear");
+const clearB = document.querySelector<HTMLButtonElement>("#clear");
 
 clearB!.addEventListener("mousedown", () => {
-  context!.fillRect(0, 0, 300, 150);
+  context.fillRect(0, 0, 300, 150);
 });
 
 let isDrawing = false;
 let x = 0;
 let y = 0;
+interface stroke {
+  x: number;
+  y: number;
+  offsetX: number;
+  offsetY: number;
+}
+const lines: stroke[] = [];
 
 canvas.addEventListener("mousedown", (e) => {
   x = e.offsetX;
@@ -36,19 +43,42 @@ canvas.addEventListener("mousedown", (e) => {
 
 canvas.addEventListener("mousemove", (e) => {
   if (isDrawing) {
-    drawLine(context, x, y, e.offsetX, e.offsetY);
+    createStroke(x, y, e.offsetX, e.offsetY);
     x = e.offsetX;
     y = e.offsetY;
+    drawStrokes(lines);
   }
 });
 window.addEventListener("mouseup", (e) => {
   if (isDrawing) {
-    drawLine(context, x, y, e.offsetX, e.offsetY);
+    createStroke(x, y, e.offsetX, e.offsetY);
     x = 0;
     y = 0;
+    drawStrokes(lines);
     isDrawing = false;
   }
 });
+
+function createStroke(
+  inX: number,
+  inY: number,
+  inOffsetX: number,
+  inOffsetY: number,
+) {
+  const line: stroke = {
+    x: inX,
+    y: inY,
+    offsetX: inOffsetX,
+    offsetY: inOffsetY,
+  };
+  lines.push(line);
+}
+function drawStrokes(toDraw: stroke[]) {
+  context.fillRect(0, 0, 300, 150);
+  toDraw.forEach((line) => {
+    drawLine(context, line.x, line.y, line.offsetX, line.offsetY);
+  });
+}
 function drawLine(
   context: CanvasRenderingContext2D,
   x1: number,
