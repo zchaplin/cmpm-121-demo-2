37a58d8cc87299ed863774dec8c29a33581c22ba
Zane Chaplin <zchaplin@ucsc.edu>
2023-10-18 11:00:21 -0700

    Step One

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
-
+app.innerHTML = '<canvas id="canvas"></canvas>';
 const gameName = "My glame";
 
 document.title = gameName;
+
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+
+const ctx = canvas.getContext("2d");
+
+ctx!.fillStyle = "green";
+
+ctx!.fillRect(10, 10, 150, 100);
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
